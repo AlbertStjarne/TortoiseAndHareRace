d985d5d971337c799a84f79fc4a0960ef6296432
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-14 18:46:28 +0100

    adds Track class with constants for length of track and for number of runners, add method for drawing the track

---
+++ _22_TortoiseAndHareRace/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _22_TortoiseAndHareRace.Properties;
+
+namespace _22_TortoiseAndHareRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var track = new Track();
+            track.DisplayRaceTrack();
+        }
+    }
+}
---
+++ _22_TortoiseAndHareRace/Properties/Track.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace _22_TortoiseAndHareRace.Properties
+{
+    public class Track
+    {
+        public int TrackLength = 10;
+        public int NumOfRunners = 2;
+
+        public string[,] Tracks { get; set; }
+
+        public Track()
+        {
+            Tracks = new string[TrackLength + 1, NumOfRunners];
+        }
+
+        public void DisplayRaceTrack()
+        {
+            Console.WriteLine();
+            for (var i = 0; i <= TrackLength; i++)
+            {
+                Console.Write(i + "  |  ");
+                for (var c = 0; c < NumOfRunners; c++)
+                {
+                    if (Tracks[i, c] == null)
+                        Console.Write("  |  ");
+                    else
+                        Console.Write(Tracks[i,c] + " | ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void RunnerPosition()
+        {
+
+        }
+
+    }
+}
